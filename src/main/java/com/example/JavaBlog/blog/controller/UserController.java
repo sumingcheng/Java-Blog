560f1fa98c02914aacec8d39345f9397d8931fe9
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-14 18:00:14 +0800

    RequestMapping

--- src/main/java/com/example/JavaBlog/blog/controller/UserController.java
+++ src/main/java/com/example/JavaBlog/blog/controller/UserController.java
@@ -2,31 +2,49 @@ package com.example.JavaBlog.blog.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    // 一个简单的用户数据库
+    private Map<String, String> users = new HashMap<>();
+
     @PostMapping("/register")
     public String register (@RequestParam String username, @RequestParam String password) {
-        // 这里添加处理用户注册的代码
+        if (users.containsKey(username)) {
+            return "Username already exists";
+        }
+        users.put(username, password);
         return "User registered";
     }
 
     @PostMapping("/login")
     public String login (@RequestParam String username, @RequestParam String password) {
-        // 这里添加处理用户登录的代码
-        return "User logged in";
+        String storedPassword = users.get(username);
+        if (storedPassword != null && storedPassword.equals(password)) {
+            return "User logged in";
+        } else {
+            return "Wrong username or password";
+        }
     }
 
     @PostMapping("/logout")
     public String logout () {
-        // 这里添加处理用户注销的代码
+        // 无状态的 REST API，无法真正地注销用户
         return "User logged out";
     }
 
     @PostMapping("/change-password")
-    public String changePassword (@RequestParam String oldPassword, @RequestParam String newPassword) {
-        // 这里添加处理用户修改密码的代码
-        return "Password changed";
+    public String changePassword (@RequestParam String username, @RequestParam String oldPassword, @RequestParam String newPassword) {
+        String storedPassword = users.get(username);
+        if (storedPassword != null && storedPassword.equals(oldPassword)) {
+            users.put(username, newPassword);
+            return "Password changed";
+        } else {
+            return "Wrong username or password";
+        }
     }
 }
