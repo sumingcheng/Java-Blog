0ae48eb7c5b5e519013c7e2b39c5b157895653b3
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-14 16:31:12 +0800

    init

---
+++ src/main/java/com/example/JavaBlog/JavaBlogApplication.java
@@ -0,0 +1,11 @@
+package com.example.JavaBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogApplication {
+    public static void main (String[] args) {
+        SpringApplication.run(JavaBlogApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/JavaBlog/JavaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
