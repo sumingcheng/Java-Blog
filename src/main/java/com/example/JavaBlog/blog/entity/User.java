730fb1729218878a600dfaa307f04952bcde7894
范晓豪 <fanxiaohaohaha@qq.com>
2023-07-14 17:16:45 +0800

    User

--- src/main/java/com/example/JavaBlog/blog/entity/User.java
+++ src/main/java/com/example/JavaBlog/blog/entity/User.java
@@ -16,5 +16,46 @@ public class User {
     private String email;
     private String role;
 
-    // getters and setters
+    public User () {
+    }
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public String getUsername () {
+        return username;
+    }
+
+    public void setUsername (String username) {
+        this.username = username;
+    }
+
+    public String getPassword () {
+        return password;
+    }
+
+    public void setPassword (String password) {
+        this.password = password;
+    }
+
+    public String getEmail () {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public String getRole () {
+        return role;
+    }
+
+    public void setRole (String role) {
+        this.role = role;
+    }
 }
